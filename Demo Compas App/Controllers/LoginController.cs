15b0420555afee7aa9f404bbb7c16a2d53ded1cb
Aniket <Aniket@ANIKET-HP>
2017-04-12 18:15:18 -0400

    Add project files.

---
+++ Demo Compas App/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using Demo_Compas_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_Compas_App.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult autherize(Demo_Compas_App.Models.User_Login usermodel)
+        {
+            using (TestEntities db = new TestEntities())
+
+            {
+               
+                var userDetails = db.User_Login.Where(x => x.Name == usermodel.Name && x.Password == usermodel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    usermodel.Loginerror = "Wrong User Name or password";
+                    return View("Index", usermodel);
+                }
+                else
+                {
+                    Session["Name"] = userDetails.Name;
+                    Session["UserId"] = userDetails.UserId;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+
+        }
+
+        public ActionResult Logout()
+        {
+            int userid=(int)Session["UserId"];
+            Session.Abandon();
+
+            return RedirectToAction("Index", "Login");
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Demo Compas App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Demo_Compas_App.MvcApplication" Language="C#" %>
